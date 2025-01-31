f158023cf536aa01b1d9d83e49e3f2326821c64a
Amro Diab <adiab@hotmail.co.uk>
2025-01-31 12:14:42 +0000

    use exit code, not return bool

--- actions/check_direction.py
+++ actions/check_direction.py
@@ -1,5 +1,4 @@
 import sys
-import subprocess
 from st2common.runners.base_action import Action
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -10,16 +9,27 @@ __all__ = ["CheckDirection"]
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class CheckDirection(Action):
+
+    @staticmethod
+    def check_result(result):
+        """
+        get exit code from bool result
+        """
+        return 0 if result else 9
+
     def run(self, direction):
-        print(f"Try to determine direction")
+        print("Try to determine direction")
         url = "http://jenkins:9090/api/v1/query"
         query = 'sum({__name__ =~ "tv_all_value_.*"})'
         value = requests.post(f'{url}?query={query}',
                               timeout=10).json()['data']['result'][0]['value'][-1]
-
+        result = False
         if direction == 'long':
-            return float(value) > 0
+            result = float(value) > 0
         if direction == 'short':
-            return float(value) < 0
+            result = float(value) < 0
         if direction == 'neutral':
-            return float(value) == 0
+            result = float(value) == 0
+
+        print(f"result is {str(result)}")
+        sys.exit(self.check_result(result))
