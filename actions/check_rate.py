7b10073b80c55018f17073f7d5d00570038baecd
Amro Diab <adiab@hotmail.co.uk>
2025-02-01 19:36:40 +0000

    fi syntax

--- actions/check_rate.py
+++ actions/check_rate.py
@@ -1,4 +1,3 @@
-#pylint: disable=syntax-error
 import sys
 from st2common.runners.base_action import Action
 import requests
@@ -21,7 +20,7 @@ class CheckRate(Action):
     def run(self, environment, direction):
         print("Check current rate")
         url = "http://jenkins:9090/api/v1/query"
-        query = f'rate({__name__=~"open_net_perc_{direction}_trades_{env}"}[10m])'
+        query = f'rate({{__name__=~"open_net_perc_{direction}_trades_{environment}"}}[10m])'
         value = requests.post(f'{url}?query={query}',
                               timeout=10).json()['data']['result'][0]['value'][-1]
         print(value)
